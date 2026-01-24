747feffb59a4bd2b41675916bee6923bfaa62f81
Aditi Sharma <aditi86017@gmail.com>
2026-01-24 09:58:09 +0530

    add lading page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import Header from "@/components/Header";
+import Providers from "@/components/Provider";
+
 
 export const metadata = {
   title: "College Event Organiser",
@@ -14,45 +15,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className=" bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white">
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-        {/* Header */}
-        <Header />
+      <body className="bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white">
+        <Providers>
+          <Header />
 
-       
-       <main className="relative min-h-screen pt-32">
-        {/*glow*/}
-        <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden"></div>
-        <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl"></div>
-        <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl"></div>
-        <div className=""></div>
+          <main className="relative min-h-screen pt-32">
+            <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+              <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl"></div>
+              <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl"></div>
+            </div>
 
-        <div className="relative z-10"></div>
+            <div className="relative z-10 min-h-[70vh]">{children}</div>
 
-
-
-  <div className="container mx-auto px-4">
-    {children}
-  </div>
-  <div className="relative z-10 min-h-[70vh]"></div>
-   {/* Footer */}
-        <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto"> {/*py= vertical padding px= horizonal padding*/}         
-        <div className="text-sm text-gray-400">Made with ❤️ by AditiCoder</div>
-        </footer>
-</main>
-</ThemeProvider>
-
-        
-        
+            <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto">
+              <div className="text-sm text-gray-400">
+                Made with ❤️ by AditiCoder
+              </div>
+            </footer>
+          </main>
+        </Providers>
       </body>
     </html>
   );
-}
+} 
+
+
 
 
 
